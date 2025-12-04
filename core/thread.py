5c59bf1fa261dc3005fe7aca2153c631daeca83c
lorenzo132 <lhoorn4@gmail.com>
2025-12-04 21:11:28 +0100

    black formatting

--- core/thread.py
+++ core/thread.py
@@ -224,12 +224,16 @@ class Thread:
                     "author_name": (
                         getattr(m.embeds[0].author, "name", "").split(" (")[0]
                         if m.embeds and m.embeds[0].author and m.author == self.bot.user
-                        else getattr(m.author, "name", None) if m.author != self.bot.user else None
+                        else getattr(m.author, "name", None)
+                        if m.author != self.bot.user
+                        else None
                     ),
                     "author_avatar": (
                         getattr(m.embeds[0].author, "icon_url", None)
                         if m.embeds and m.embeds[0].author and m.author == self.bot.user
-                        else m.author.display_avatar.url if m.author != self.bot.user else None
+                        else m.author.display_avatar.url
+                        if m.author != self.bot.user
+                        else None
                     ),
                 }
                 async for m in channel.history(limit=None, oldest_first=True)
@@ -254,7 +258,7 @@ class Thread:
         import logging
 
         logging.info(f"[SNOOZE] DB update result: {result.modified_count}")
-        
+
         # Dispatch thread_snoozed event for plugins
         self.bot.dispatch("thread_snoozed", self, moderator, snooze_for)
 
@@ -749,7 +753,7 @@ class Thread:
 
         # Mark unsnooze as complete
         self._unsnoozing = False
-        
+
         # Dispatch thread_unsnoozed event for plugins
         self.bot.dispatch("thread_unsnoozed", self)
 
