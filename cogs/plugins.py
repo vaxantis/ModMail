c0db4c8f9376193f6086cdf4df4be40b0c1919fa
Boo <99917823+Boopup@users.noreply.github.com>
2025-12-28 23:44:22 -0600

    Update plugin URL to be the new Modmail Docs website
    Signed-off-by: Boo <99917823+Boopup@users.noreply.github.com>

--- cogs/plugins.py
+++ cogs/plugins.py
@@ -113,7 +113,7 @@ class Plugins(commands.Cog):
     These addons could have a range of features from moderation to simply
     making your life as a moderator easier!
     Learn how to create a plugin yourself here:
-    https://github.com/modmail-dev/modmail/wiki/Plugins
+    https://docs.modmail.dev/usage-guide/plugins
     """
 
     def __init__(self, bot):
